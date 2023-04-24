13996d65334f784303ee812c72b215bb45b5eabb
ljylwnxx <ljylwnxx@163.com>
2023-04-24 15:19:54 +0800

    chore:build

--- config/index.js
+++ config/index.js
@@ -1,6 +1,19 @@
+// const path = require('path')
 export default {
     baseUrl:{
         dev:'/api',
-        pro:''
-    }
+        pro:'',
+        assetsPublicPath: '/'   
+    },
+    // build: {
+    //     // Template for index.html
+    //     index: path.resolve(__dirname, '../dist/index.html'),
+    
+    //     // Paths
+    //     assetsRoot: path.resolve(__dirname, '../dist'),
+    //     assetsSubDirectory: 'static',
+    //     assetsPublicPath: './'
+        
+    //    // 下面的省略 
+    // }    
 }
